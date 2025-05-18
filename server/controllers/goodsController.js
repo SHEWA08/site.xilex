2210d571ddcf9d242aa38ba65dfe22ad7a31b20e
SHEWA08 <shevart07@gmail.com>
2025-05-18 16:38:42 +0300

    Function for receiving all goods

--- server/controllers/goodsController.js
+++ server/controllers/goodsController.js
@@ -5,12 +5,12 @@ const ApiError = require('../error/ApiError')
 class GoodsController {
     async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, info} = req.body
-        const {img} = req.files
-        let filename = uuid.v4() + ".jpg"
-        img.mv(path.resolve(__dirname, '..', 'static', filename))
+            const {name, price, brandId, typeId, info, size} = req.body
+        const img = req.files.img
+        let fileName = uuid.v4() + ".jpg"
+        img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
-        const goods = await Goods.create({name, price, brandId, typeId, size,  img: filename})
+        const goods = await Goods.create({name, price, brandId, typeId, size,  img: fileName, info})
 
         return res.json(goods)
 
@@ -21,8 +21,27 @@ class GoodsController {
     }
 
     async getAll(req, res) {
+        const {brandId, typeId} = req.query
+        let goods;
+        if (!brandId && !typeId) {
+            goods = await Goods.findAll()
 
+        }
+        if(brandId && !typeId) {
+            goods = await Goods.findAll({where:{brandId}})
 
+        }
+
+        if(typeId && !brandId) {
+            goods = await Goods.findAll({where:{typeId}})
+            
+        }
+
+        if(brandId && typeId) {
+            goods = await Goods.findAll({where:{brandId, typeId}})
+            
+        }
+        return res.json(goods)
     }
     async getOne(req, res) {
 
